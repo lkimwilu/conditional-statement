74b9cd7509a68f9cd1e98bb18c24a72df334a572
lkim <lkimwilu@gmail.com>
2024-07-04 21:07:18 +0300

    Adds conditional statements

---
+++ conditional.js
@@ -0,0 +1,22 @@
+// Create a score variable.
+
+// Loop through conditional statements and print results from A - F based on results.
+
+// let score = 80;
+
+
+let score = 80;
+
+if (score >= 90 ) {
+    console.log('You Got ');
+} else if (score >= 80 ) {
+    console.log('You Got B');
+} else if (score >= 70) {
+    console.log('You Got C');
+} else if (score >= 60) {
+    console.log('You Got D');
+} else if (score >= 0) {
+    console.log('You Got F');
+} else {
+    console.log('Invalid score');
+}
